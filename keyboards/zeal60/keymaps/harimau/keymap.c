692f73eb4061c8050fb082b582fc3db5d99361b0
James Laird-Wah <james@laird-wah.net>
2018-10-23 11:40:54 +1100

    zeal60:harimau add consumer control

--- keyboards/zeal60/keymaps/harimau/keymap.c
+++ keyboards/zeal60/keymaps/harimau/keymap.c
@@ -6,6 +6,7 @@
 enum {
   DEF = 0,
   RAI,
+  LOW,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -14,13 +15,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 	KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT,
-	KC_LCTL, KC_LGUI, KC_LALT,                            LT(RAI, KC_SPC),                    KC_BSPC, KC_ENT,  _______, KC_RALT),
+	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC, KC_RALT,
+	KC_LCTL, KC_LGUI, KC_LALT,                            LT(RAI, KC_SPC),                    KC_ENT,  _______, _______, MO(LOW)),
 
 [RAI] = LAYOUT(
 	KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL ,
 	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
 	_______, _______, _______, _______, _______, KC_SPC,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, RESET,
+	_______, _______, _______,                            _______,                            _______, _______, _______, _______),
+
+[LOW] = LAYOUT(
+	_______, KC_SBRD, KC_SBRU, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______,                            _______,                            _______, _______, _______, _______),
 };
